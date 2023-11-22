4d7ba05c8877a0f37d2b856600591d3ad90a77ac
Pedro Castilhos <79242129+PedroCastilhos@users.noreply.github.com>
2023-11-22 15:23:34 -0300

    dados

---
+++ vendas/public/equipamentos.js
@@ -0,0 +1,46 @@
+export const dadosEquipamentos = [
+  {
+    id: 1,
+    tipo: 'Grades - Niveladoras de Arrasto',
+    marca: 'Tatu Marchesan',
+    modelo: 'GTD 24x24',
+    larguraTrabalho: '6 metros',
+    preco: 35000,
+    foto: 'tatu_marchesan_gtd_24x24.jpg',
+    descricao:
+      'As grades niveladoras de arrasto GTD 24x24 da Tatu Marchesan são ideais para preparo de solo pós-plantio, nivelamento e incorporação de restos vegetais. Com 24 discos de 24 polegadas, oferecem eficiência e uniformidade no trabalho.',
+  },
+  {
+    id: 2,
+    tipo: 'Aradoras - Grades',
+    marca: 'Jumil',
+    modelo: 'JMPA 40x40',
+    larguraTrabalho: '5 metros',
+    preco: 28000,
+    foto: 'jumil_jmpa_40x40.jpg',
+    descricao:
+      'O conjunto de aradoras com grades JMPA 40x40 da Jumil é projetado para o preparo do solo antes do plantio. Com discos de corte e hastes de arrasto, proporcionam um trabalho eficiente na quebra de torrões e nivelamento do terreno.',
+  },
+  {
+    id: 3,
+    tipo: 'Enfardadora Prismática',
+    marca: 'New Holland',
+    modelos: ['328', '338', '348'],
+    tipoEnfardadora: 'Prismática',
+    preco: 55000,
+    foto: 'new_holland_enfardadora_prismatica.jpg',
+    descricao:
+      'A linha de enfardadoras prismáticas da New Holland, modelos 328, 338 e 348, oferece alta performance na produção de fardos retangulares. Com sistema de prensagem avançado, são ideais para feno, palha e forragem.',
+  },
+  {
+    id: 4,
+    tipo: 'Enfardadora Prismática Grande',
+    marca: 'John Deere',
+    modelo: 'L341',
+    tamanhoFardo: '90x120 centímetros',
+    preco: 65000,
+    foto: 'john_deere_l341.jpg',
+    descricao:
+      'A enfardadora prismática grande L341 da John Deere é ideal para produção de fardos maiores. Com capacidade de fardos retangulares de 90x120 centímetros, oferece alta densidade e eficiência na colheita de forragem.',
+  },
+];
